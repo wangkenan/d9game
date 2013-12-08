4735528f6f4772b78b9f3af1811025cb06428159
jichaoqin <jichaoqin@duobaohui.com>
2013-12-08 16:15:11 +0800

    解决下载管理中应用名换行，更新游戏中游戏大小显示错误的问题

--- src/me/key/appmarket/adapter/UpdataAdapter.java
+++ src/me/key/appmarket/adapter/UpdataAdapter.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import me.key.appmarket.MarketApplication;
 import me.key.appmarket.tool.DownloadService;
+import me.key.appmarket.tool.ToolHelper;
 import me.key.appmarket.utils.AppInfo;
 import me.key.appmarket.widgets.ProgressView;
 
@@ -29,27 +30,27 @@ import android.widget.Toast;
 
 /**
  * 可升级应用adapter
+ * 
  * @author Administrator
- *
+ * 
  */
 public class UpdataAdapter extends BaseAdapter {
 	private LayoutInflater mInflater;
 	private Context context;
 	private List<AppInfo> updataList;
-	private DisplayImageOptions options = new DisplayImageOptions.Builder()  
-    .showImageForEmptyUri(R.drawable.tempicon).showStubImage(R.drawable.tempicon)  
-    .resetViewBeforeLoading(false) 
-    .delayBeforeLoading(100)  
-    .cacheInMemory(true)           
-    .cacheOnDisc(true)              
-    .imageScaleType(ImageScaleType.IN_SAMPLE_INT)
-    .bitmapConfig(Bitmap.Config.RGB_565)               
-    .build(); 
-	public UpdataAdapter(List<AppInfo> updataList,Context context){
+	private DisplayImageOptions options = new DisplayImageOptions.Builder()
+			.showImageForEmptyUri(R.drawable.tempicon)
+			.showStubImage(R.drawable.tempicon).resetViewBeforeLoading(false)
+			.delayBeforeLoading(100).cacheInMemory(true).cacheOnDisc(true)
+			.imageScaleType(ImageScaleType.IN_SAMPLE_INT)
+			.bitmapConfig(Bitmap.Config.RGB_565).build();
+
+	public UpdataAdapter(List<AppInfo> updataList, Context context) {
 		this.updataList = updataList;
 		mInflater = LayoutInflater.from(context);
 		this.context = context;
 	}
+
 	@Override
 	public int getCount() {
 		return updataList.size();
@@ -68,26 +69,28 @@ public class UpdataAdapter extends BaseAdapter {
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
 		ViewHolder holder;
-		final AppInfo sdappInfo = updataList.get(position);;
-		if(convertView == null) {
+		final AppInfo sdappInfo = updataList.get(position);
+		;
+		if (convertView == null) {
 			holder = new ViewHolder();
-			convertView = mInflater.inflate(R.layout.updata_manager_list_item, null);
+			convertView = mInflater.inflate(R.layout.updata_manager_list_item,
+					null);
 			holder.info = (TextView) convertView.findViewById(R.id.info);
 			holder.icon = (ImageView) convertView.findViewById(R.id.icon);
-			holder.operate = (ImageView) convertView.findViewById(R.id.iv_oprate_state);
+			holder.operate = (ImageView) convertView
+					.findViewById(R.id.iv_oprate_state);
 			holder.btn = (TextView) convertView.findViewById(R.id.install);
 			holder.progress_view = (ProgressView) convertView
 					.findViewById(R.id.progress_view_local);
 
-			holder.appsize = (TextView) convertView
-					.findViewById(R.id.appsize);
+			holder.appsize = (TextView) convertView.findViewById(R.id.appsize);
 			convertView.setTag(holder);
 		} else {
-			holder= ((ViewHolder) convertView.getTag());
-			
+			holder = ((ViewHolder) convertView.getTag());
+
 		}
 		boolean isUpdate = false;
-		
+
 		holder.btn.setText("升级");
 		holder.progress_view.setProgress(100);
 		holder.operate.setImageResource(R.drawable.update_btn);
@@ -98,59 +101,61 @@ public class UpdataAdapter extends BaseAdapter {
 		 * null, null);
 		 */
 		/*
-		 * else { sdappInfo = mData .get((newposition - appManaInfos.size()
-		 * - downList .size()));
+		 * else { sdappInfo = mData .get((newposition - appManaInfos.size() -
+		 * downList .size()));
 		 * 
 		 * Drawable mDrawable2 = cnt.getResources().getDrawable(
 		 * R.drawable.downloaded);
 		 * v2.btn.setCompoundDrawablesWithIntrinsicBounds(null, mDrawable2,
 		 * null, null);
 		 * 
-		 * v2.btn.setText("安装"); v2.progress_view.setProgress(0); String mb
-		 * = ToolHelper.Kb2Mb(sdappInfo.getAppSize());
-		 * v2.appsize.setText(mb); }
+		 * v2.btn.setText("安装"); v2.progress_view.setProgress(0);
 		 */
+
+		String mb = ToolHelper.Kb2Mb(sdappInfo.getAppSize());
+		holder.appsize.setText(mb);
 		holder.btn.setOnClickListener(new OnClickListener() {
 
-				@Override
-				public void onClick(View v) {
-					List<AppInfo> down_temp = new ArrayList<AppInfo>();
-					File tempFile = new File(Environment
-							.getExternalStorageDirectory(), "/market/"
-							+ sdappInfo.getAppName() + ".apk");
-					if (tempFile.exists()) {
-						tempFile.delete();
-					}
-					DownloadService.downNewFile(sdappInfo, 0, 0, null);
-					MarketApplication.getInstance().getDownApplist().add(sdappInfo);
-					notifyDataSetChanged();
-					sdappInfo.setDown(true);
-					sdappInfo.setIspause(false);
-					Intent intent = new Intent();
-					Intent downState = new Intent();
-
-					downState.setAction(tempFile.getAbsolutePath() + "down");
-					downState.putExtra("isPause", sdappInfo.isIspause());
-					context.sendBroadcast(downState);
-					intent.setAction(MarketApplication.PRECENT);
-					context.sendBroadcast(intent);
-					Toast.makeText(context,
-							sdappInfo.getAppName() + " 开始下载...",
-							Toast.LENGTH_SHORT).show();
-
+			@Override
+			public void onClick(View v) {
+				List<AppInfo> down_temp = new ArrayList<AppInfo>();
+				File tempFile = new File(Environment
+						.getExternalStorageDirectory(), "/market/"
+						+ sdappInfo.getAppName() + ".apk");
+				if (tempFile.exists()) {
+					tempFile.delete();
 				}
-			});
+				DownloadService.downNewFile(sdappInfo, 0, 0, null);
+				MarketApplication.getInstance().getDownApplist().add(sdappInfo);
+				notifyDataSetChanged();
+				sdappInfo.setDown(true);
+				sdappInfo.setIspause(false);
+				Intent intent = new Intent();
+				Intent downState = new Intent();
+
+				downState.setAction(tempFile.getAbsolutePath() + "down");
+				downState.putExtra("isPause", sdappInfo.isIspause());
+				context.sendBroadcast(downState);
+				intent.setAction(MarketApplication.PRECENT);
+				context.sendBroadcast(intent);
+				Toast.makeText(context, sdappInfo.getAppName() + " 开始下载...",
+						Toast.LENGTH_SHORT).show();
+
+			}
+		});
 		holder.progress_view.setProgress(0);
 		// v2.progress_view.setVisibility(View.VISIBLE);
 		holder.info.setText(sdappInfo.getAppName());
 		holder.icon.setVisibility(View.VISIBLE);
-		ImageLoader.getInstance().displayImage(sdappInfo.getIconUrl(), holder.icon, options);
+		ImageLoader.getInstance().displayImage(sdappInfo.getIconUrl(),
+				holder.icon, options);
 		return convertView;
 	}
+
 	static class ViewHolder {
 		public TextView appsize;
 		public TextView btn;
-		public ImageView icon,operate;
+		public ImageView icon, operate;
 		public TextView info;
 		private ProgressView progress_view;
 	}
